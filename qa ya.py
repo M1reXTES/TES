de8908b09cb77dc0b290179a34e34e77c2d94daf
estvorogov <109388765+estvorogov@users.noreply.github.com>
2023-03-23 19:37:40 +0300

    Update qa ya.py

--- qa ya.py
+++ qa ya.py
@@ -12,20 +12,25 @@ base_url = 'https://passport.yandex.ru/auth?mode=add-user&retpath=%2F%2Fyandex.r
 driver.get(base_url)
 driver.maximize_window()
 
+
 def click(xpath):
     click_1 = driver.find_element(By.XPATH, xpath)
     click_1.click()
+
+
 def send_text(text, xpath):
     text_in = driver.find_element(By.XPATH, xpath)
     text_in.clear()
     text_in.send_keys(text)
+
+
 def double_click(xpath):
     action = ActionChains(driver)
-    new_push = driver.find_element(By.XPATH,xpath)
+    new_push = driver.find_element(By.XPATH, xpath)
     action.double_click(new_push).perform()
 
 
-send_text('lisasumina@yandex.ru','//*[@id="passp-field-login"]')
+send_text('lisasumina@yandex.ru', '//*[@id="passp-field-login"]')
 click('//*[@id="passp:sign-in"]')
 send_text('Qwerty218805', '//*[@id="passp-field-passwd"]')
 click('//*[@id="passp:sign-in"]')
@@ -37,18 +42,21 @@ time.sleep(5)
 
 click("//*[@id='app']/div/div/div[3]/div[1]/div[1]/div/div/span[2]/button")
 click('//*[@id="nb-1"]/body/div[3]/div/button[1]/span[1]')
-send_text('Test', '//*[@id="nb-1"]/body/div[4]/div[2]/div/div/div/div/div/div[1]/div/form/span/input')
+send_text(
+    'Test', '//*[@id="nb-1"]/body/div[4]/div[2]/div/div/div/div/div/div[1]/div/form/span/input')
 click('//*[@id="nb-1"]/body/div[4]/div[2]/div/div/div/div/div/div[2]/button')
 time.sleep(3)
 
-double_click('//*[@id="app"]/div/div/div[3]/div[2]/div[1]/div/div/div[3]/div/div[1]/div[2]/div/div')
+double_click(
+    '//*[@id="app"]/div/div/div[3]/div[2]/div[1]/div/div/div[3]/div/div[1]/div[2]/div/div')
 time.sleep(2)
 click('//*[@id="app"]/div/div/div[3]/div[1]/div[1]/div/div/span[2]/button')
 click('//*[@id="nb-1"]/body/div[3]/div/button[3]/span[1]')
 time.sleep(1)
-send_text('Fail', '//*[@id="nb-1"]/body/div[4]/div[2]/div/div/div/div/div/div[1]/div/form/span/input')
+send_text(
+    'Fail', '//*[@id="nb-1"]/body/div[4]/div[2]/div/div/div/div/div/div[1]/div/form/span/input')
 click('//*[@id="nb-1"]/body/div[4]/div[2]/div/div/div/div/div/div[2]/button')
 time.sleep(10)
-driver.close()
-driver.back()
 
+return_folder = 'https://disk.yandex.ru/client/disk/Test'
+driver.get(return_folder)
